db79abe2f9063728d22fe6094549a22c38d0d80f
anpabeltj <patrapembinaan@gmail.com>
2025-02-08 16:35:00 +0800

    fix: fix identation

--- contact/index.js
+++ contact/index.js
@@ -1,1 +1,11 @@
-const initialContacts = [{ id: 1, fullName: "John Doe", email: "johndoe@gmail.com", phoneNUmber: +628122293383, label: "friend", birthDate: 1990 - 25 - 6, notes: "" }];
+const initialContacts = [
+  {
+    id: 1,
+    fullName: "John Doe",
+    email: "johndoe@gmail.com",
+    phoneNUmber: +628122293383,
+    label: "friend",
+    birthDate: 1990 - 25 - 6,
+    notes: "",
+  },
+];
