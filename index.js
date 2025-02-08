be4c50eb0036af15f689ff00e3380c7e834391e3
M Haidar Hanif <git@mhaidarhanif.com>
2025-02-08 15:55:34 +0700

    feat: data contacts into main index.js and add more contacts

---
+++ index.js
@@ -0,0 +1,29 @@
+let dataContacts = [
+  {
+    id: 1,
+    fullName: "Anpabelt",
+    email: "anpabelt@example.com",
+    phoneNumber: "+60-2293-383",
+    labels: ["friend", "neighbour"],
+    birthDate: new Date("1990-25-6"),
+    notes: "Studied together at the university",
+  },
+  {
+    id: 2,
+    fullName: "Trah",
+    email: "trah@example.com",
+    phoneNumber: "+60-1234-383",
+    labels: ["coworker"],
+    birthDate: new Date("1995-25-6"),
+    notes: "Working in restaurant",
+  },
+  {
+    id: 3,
+    fullName: "Javala",
+    email: "javala@example.com",
+    phoneNumber: "+60-4567-383",
+    labels: ["family"],
+    birthDate: new Date("1992-25-6"),
+    notes: "My cousin",
+  },
+];
