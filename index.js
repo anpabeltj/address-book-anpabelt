11d7f55e479572a7d526cffbe28d8b9f4b7ac3c3
M Haidar Hanif <git@mhaidarhanif.com>
2025-02-08 16:44:30 +0700

    feat: add more contact's fields

--- index.js
+++ index.js
@@ -1,30 +1,45 @@
+console.log(`============
+Address Book
+============`);
+
 let dataContacts = [
   {
     id: 1,
+    avatarUrl: "https://github.com/anpabeltj.png",
+    url: "https://anpabelt.com",
     fullName: "Anpabelt",
     email: "anpabelt@example.com",
     phoneNumber: "+60-2293-383",
     labels: ["friend", "neighbour"],
-    birthDate: new Date("1990-25-6"),
+    birthDate: new Date("1990-6-25"),
     notes: "Studied together at the university",
+    address: {
+      street: "Jalan 1",
+      city: "Kuala Lumpur",
+      state: "Kuala Lumpur",
+      postalCode: "56000",
+      country: "Malaysia",
+    },
   },
   {
     id: 2,
+    avatarURL: "https://github.com/anpabeltj.png",
     fullName: "Trah",
     email: "trah@example.com",
     phoneNumber: "+60-1234-383",
     labels: ["coworker"],
-    birthDate: new Date("1995-25-6"),
+    birthDate: new Date("1995-2-20"),
     notes: "Working in restaurant",
   },
   {
     id: 3,
+    avatarURL: "https://github.com/anpabeltj.png",
     fullName: "Javala",
     email: "javala@example.com",
     phoneNumber: "+60-4567-383",
     labels: ["family"],
-    birthDate: new Date("1992-25-6"),
-    notes: "My cousin",
+    birthDate: new Date("1992-10-12"),
+    notes: null,
   },
 ];
 
